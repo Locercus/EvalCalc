1dfe3ff0d2a22dfc9376f01f480097c55f2ccd15
Creeps <creeper32605@gmail.com>
2015-04-10 22:21:42 +0200

    Variables can now be removed. Almost.

--- ui.js
+++ ui.js
@@ -1,11 +1,20 @@
+var addedVariable = function(){};
+var onVariableRemove = function(){};
 $(document).ready(function(){
+	var reqFrame = window.requestAnimationFrame || window.webkitRequestAnimationFrame || function(f){setTimeout(f,17)};
+	
 	onStorageReady(function(){
 		$("#controls").removeClass("pending");
 		
 		if( storage.data.degrad ) {
 			if( storage.data.degrad == "deg" ) {
 				$(".toggle-button").addClass('switch').attr('value','deg');
+				setAngles("deg");
+			} else {
+				setAngles("rad");
 			}
+		} else {
+			setAngles("rad");
 		}
 	});
 	initStorage();
@@ -17,7 +26,63 @@ $(document).ready(function(){
 		if( $(this).hasClass('switch')) {
 			value = $(this).find('.toggle-item:last-child').attr('value');
 		}
+		setAngles(value);
 		storage.data.degrad = value;
 		$(this).attr('value', value );
 	});
+	
+	var variableRemoveListeners = [];
+	onVariableRemove = function(f) {
+		if( typeof f != 'function' ) {
+			throw new Error("Argument 0 must be a function");
+		}
+		variableRemoveListeners.push(f);
+	}
+	var callRemoveVariable = function(k) {
+		for( var i in variableRemoveListeners ) {
+			try {
+				variableRemoveListeners[i](k);
+			} catch(baby) {
+				throw baby;
+			}
+		}
+	}
+	
+	var eventVariableRemoveClick = function(e) {
+		var variable = $(this).parent();
+		variable.css({
+			'-webkit-transform-origin': '50% 0',
+			'-moz-transform-origin': '50% 0',
+			'-ms-transform-origin': '50% 0',
+			'-o-transform-origin': '50% 0',
+			'transform-origin': '50% 0',
+			'-webkit-transition': '-webkit-transform .3s cubic-bezier(.2,.3,0,1)',
+			'-o-transition': '-o-transform .3s cubic-bezier(.2,.3,0,1)',
+			'transition': 'transform .3s cubic-bezier(.2,.3,0,1)'
+		});
+		reqFrame(function(){
+			variable.css({
+				'-webkit-transform': 'scaleY(0)',
+				'-ms-transform': 'scaleY(0)',
+				'-o-transform': 'scaleY(0)',
+				'transform': 'scaleY(0)'
+			}).on('transitionend webkitTransitionEnd oTransitionEnd', function(){
+				var key = variable.data('key');
+				variable.remove();
+				callRemoveVariable(key);
+			});
+		});
+	}
+	
+	var eventVariableTouchStart = function(e){
+		
+	}
+	
+	addedVariable = function(obj) {
+		if( typeof obj != 'object' ) {
+			throw new Error("Argument 0 must be an object");
+		}
+		var variable = $(obj);
+		variable.find(".variable-remove").on('click', eventVariableRemoveClick);
+	}
 });
