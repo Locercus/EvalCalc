c15feb0f523942510b36a4218f08e57f6f7d5690
Creeps <creeper32605@gmail.com>
2015-04-18 09:54:59 +0200

    Change shortcut to (ctrl|cmd) + .

--- scripts/cmd.js
+++ scripts/cmd.js
@@ -1,14 +1,23 @@
 $(document).ready(function(){
 	$(document).keydown(function(e){
-		// 91: left cmd key
-		// 93: right cmd key
-		if( e.which == 91 || e.which == 93 ) {
+		// 190 = period key (on osx, don't know about windows)
+		if( e.which == 190 && ( e.metaKey || e.ctrlKey ) ) {
+			e.preventDefault();
 			$("#cmd-line").toggleClass('hidden');
 			if( $("#cmd-line").hasClass('hidden') ) {
 				$("#input").focus();
 			} else {
 				$("#cmd-input").focus();
 			}
+		}
+	});
+	$("#cmd-input").on('input', function(){
+		var r = "";
+		try {
+			r = math.eval($("#cmd-input").val());
+		} catch(err){}
+		if( r ) {
+			$("#cmd-results").text(r);
 		}
 	});
 });
