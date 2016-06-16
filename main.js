0a47e579d91aff1ccf0e51547b04ed5b0a3fb9c7
Mia Nordentoft <mia@miestasmia.com>
2016-06-16 15:43:57 +0200

    Update name

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 /*
- * Copyright 2015 Jonatan Nordentoft, Creeper32605
+ * Copyright 2015 Mia Nordentoft, Creeper32605
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
